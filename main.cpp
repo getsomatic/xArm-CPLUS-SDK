b400a859d235ffeaf4a115fe82b2b9c8ce0b7a5d
Alex Kovalchuk <olexandr@getsomatic.com>
2021-06-02 14:53:29 +0300

    added ament package

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 
 #include<xarm/wrapper/xarm_api.h>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-
     auto api = XArmAPI("192.168.1.200");
     api.core->set_mode(2);
     api.core->set_state(0);
 
+    while (api.is_connected()) {
+        int cmd;
+        cout << "Cin command\n";
+        cin >> cmd;
+        api.core->set_mode(cmd);
+        api.core->set_state(0);
+    }
+    cout << "Disconnected\n";
 
     return 0;
 }
